5efd25d60eb9857a8408d53aa8815fb35fcd8508
szabinakov <szabinkov@gmail.com>
2020-08-26 14:49:40 +0100

    Added Alert Component to Properties

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -2,23 +2,32 @@ import React, { useState, useEffect } from 'react'
 import PropertyCard from './PropertyCard'
 import axios from 'axios'
 import '../styles/Properties.css'
-
+import Alert from '../components/Alert'
 
 const Properties = () => {
+
+    const initialState = {
+        alert: {
+            message: ''
+        }
+    }
     const [properties, setProperties] = useState([])
+    const [alert, setAlert] = useState(initialState.alert)
 
     useEffect(()=>{
         axios.get('http://localhost:4000/api/v1/PropertyListing')
         .then(response => {
             setProperties(response.data)
         })
-        .catch(err =>{
-            console.log(err)
+        .catch(() =>{
+            setAlert({message: 'Something went wrong!'})
         })
     },[])
+
     return (
         <>
         <h2>Properties</h2>
+        {alert.message && <Alert message={alert.message}/>} 
         <PropertyCard details={properties}/>
         </>
     )
--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -47,37 +47,8 @@ const PropertyCard = ({details}) => {
 
 }
 
+
+
 export default PropertyCard
 
 
-// return (
-//     <div className='PropertyCard'>
-//         <>{eachProperty}</>
-//          <img src={PlaceholderPic} alt='Placeholder Property'/>
-//         <div className='PropertyDetailsDiv'>
-//         <div data-testid='titleId' className='titleDiv'>
-//             {details.title}
-//         </div>
-//         <div data-testid='typeId' className='typeDiv'>
-//             <FontAwesomeIcon className='icon' icon={faHome}/>{details.type}
-//         </div>
-//         <div data-testid='bedroomsId' className='bedroomsDiv'>
-//             <FontAwesomeIcon className='icon' icon={faBed}/>{details.bedrooms}
-//         </div>
-//         <div data-testid='bathroomsId' className='bathroomsDiv'>
-//             <FontAwesomeIcon className='icon' icon={faBath}/>{details.bathrooms}
-//         </div>
-//         <div data-testid='priceId' className='priceDiv'>
-//             <FontAwesomeIcon className='icon' icon={faPoundSign}/>{details.price}
-//         </div>
-//         <div data-testid='cityId' className='cityDiv'>
-//             <FontAwesomeIcon className='icon' icon={faCity}/>{details.city}
-//         </div>
-//         <div data-testid='emailId' className='emailDiv'>
-//             <FontAwesomeIcon className='icon' icon={faEnvelope}/>
-//             <a href={details.email}>Send Email!</a>
-//         </div>
-//         </div> 
-//     </div>
-    
-// )
