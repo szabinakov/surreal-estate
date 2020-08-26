cf4d549300c0562e14e71a616a43f377400302fb
szabinakov <szabinkov@gmail.com>
2020-08-26 14:14:30 +0100

    Changed dummy data to real in PRoperty Cards, and added some styling

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropertyCard from './PropertyCard'
+import axios from 'axios'
+import '../styles/Properties.css'
+
 
 const Properties = () => {
+    const [properties, setProperties] = useState([])
+
+    useEffect(()=>{
+        axios.get('http://localhost:4000/api/v1/PropertyListing')
+        .then(response => {
+            setProperties(response.data)
+        })
+        .catch(err =>{
+            console.log(err)
+        })
+    },[])
     return (
         <>
-        <PropertyCard/>
+        <h2>Properties</h2>
+        <PropertyCard details={properties}/>
         </>
     )
 }
--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -4,47 +4,80 @@ import '../styles/PropertyCard.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faBed, faBath, faPoundSign, faCity, faHome, faEnvelope} from '@fortawesome/free-solid-svg-icons'
 
-const PropertyCard = () => {
-    const details = {
-        title:'1 Bed Flat',
-        type: 'Flat',
-        bedrooms: 1,
-        bathrooms: 2,
-        price: 12300,
-        city: 'Leeds',
-        email: 'this.is@theemail'
-    }
+const PropertyCard = ({details}) => {
+
+    const eachProperty = details.map((property, index) => 
+            <div className='EachProperty' key={index}>
+                <img src={PlaceholderPic} alt='Placeholder Property'/>
+                <div data-testid='titleId' className='titleDiv'>
+                    {property.title}
+                </div>
+                <div data-testid='typeId' className='typeDiv'>
+                    <FontAwesomeIcon className='icon' icon={faHome}/>
+                    {property.type}
+                </div>
+                <div data-testid='bedroomsId' className='bedroomsDiv'>
+                    <FontAwesomeIcon className='icon' icon={faBed}/>
+                    {property.bedrooms}
+                </div>
+                <div data-testid='bathroomsId' className='bathroomsDiv'>
+                    <FontAwesomeIcon className='icon' icon={faBath}/>
+                    {property.bathrooms}
+                </div>
+                <div data-testid='priceId' className='priceDiv'>
+                    <FontAwesomeIcon className='icon' icon={faPoundSign}/>
+                    {property.price}
+                </div>
+                <div data-testid='cityId' className='cityDiv'>
+                    <FontAwesomeIcon className='icon' icon={faCity}/>
+                    {property.city}
+                </div>
+                <div data-testid='emailId' className='emailDiv'>
+                    <FontAwesomeIcon className='icon' icon={faEnvelope}/>
+                    <a href={property.email}>Send Email!</a>
+                </div>
+            </div>
+            )
+
     return (
-        <div className='PropertyCard'>
-            <img src={PlaceholderPic} alt='Placeholder Property'/>
-            <div className='PropertyDetailsDiv'>
-            <div data-testid='titleId' className='titleDiv'>
-                {details.title}
-            </div>
-            <div data-testid='typeId' className='typeDiv'>
-                <FontAwesomeIcon className='icon' icon={faHome}/>{details.type}
-            </div>
-            <div data-testid='bedroomsId' className='bedroomsDiv'>
-                <FontAwesomeIcon className='icon' icon={faBed}/>{details.bedrooms}
-            </div>
-            <div data-testid='bathroomsId' className='bathroomsDiv'>
-                <FontAwesomeIcon className='icon' icon={faBath}/>{details.bathrooms}
-            </div>
-            <div data-testid='priceId' className='priceDiv'>
-                <FontAwesomeIcon className='icon' icon={faPoundSign}/>{details.price}
-            </div>
-            <div data-testid='cityId' className='cityDiv'>
-                <FontAwesomeIcon className='icon' icon={faCity}/>{details.city}
-            </div>
-            <div data-testid='emailId' className='emailDiv'>
-                <FontAwesomeIcon className='icon' icon={faEnvelope}/>
-                <a href={details.email}>Send Email!</a>
-            </div>
-            </div>
+        <div className='PropertyContainer'>
+            {eachProperty}
         </div>
-        
     )
 
 }
 
 export default PropertyCard
+
+
+// return (
+//     <div className='PropertyCard'>
+//         <>{eachProperty}</>
+//          <img src={PlaceholderPic} alt='Placeholder Property'/>
+//         <div className='PropertyDetailsDiv'>
+//         <div data-testid='titleId' className='titleDiv'>
+//             {details.title}
+//         </div>
+//         <div data-testid='typeId' className='typeDiv'>
+//             <FontAwesomeIcon className='icon' icon={faHome}/>{details.type}
+//         </div>
+//         <div data-testid='bedroomsId' className='bedroomsDiv'>
+//             <FontAwesomeIcon className='icon' icon={faBed}/>{details.bedrooms}
+//         </div>
+//         <div data-testid='bathroomsId' className='bathroomsDiv'>
+//             <FontAwesomeIcon className='icon' icon={faBath}/>{details.bathrooms}
+//         </div>
+//         <div data-testid='priceId' className='priceDiv'>
+//             <FontAwesomeIcon className='icon' icon={faPoundSign}/>{details.price}
+//         </div>
+//         <div data-testid='cityId' className='cityDiv'>
+//             <FontAwesomeIcon className='icon' icon={faCity}/>{details.city}
+//         </div>
+//         <div data-testid='emailId' className='emailDiv'>
+//             <FontAwesomeIcon className='icon' icon={faEnvelope}/>
+//             <a href={details.email}>Send Email!</a>
+//         </div>
+//         </div> 
+//     </div>
+    
+// )
