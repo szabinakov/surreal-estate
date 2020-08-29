fe110509394715540b428760dd3ca4e484635785
szabinakov <szabinkov@gmail.com>
2020-08-29 17:06:21 +0100

    added option Searchbar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,34 +1,74 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState } from 'react'
+import {Link, useLocation, useHistory} from 'react-router-dom'
 import '../styles/SideBar.css'
+import qs from 'qs'
+
 
 const SideBar = () => {
+    const [query, setQuery ] = useState('')
+    const { search } = useLocation()
+    const history = useHistory()
+    
+    const buildQueryString = (operation, valueObj) => {
+        
+        const currentQueryParams = qs.parse(search, { ignoreQueryPrefix : true } )
+        
+        const newQueryParams = {
+            ...currentQueryParams,
+             [operation]: JSON.stringify({
+                 ...JSON.parse(currentQueryParams[operation] || '{}'),
+                 ...valueObj,
+             })
+        }
+        return qs.stringify(newQueryParams, { addQueryPrefix: true, encode : false })
+    }
+
+    const handleSearch = (event) => {
+        event.preventDefault()
+        const newQuery = buildQueryString('query', { title: { $regex: query } })
+    
+        history.push(newQuery)
+    }
 
     return (
 
         <div className='SideBar'>
+            <form onSubmit={handleSearch} className='SearchForm'>
+                <input 
+                    value={query}
+                    onChange={(event) => setQuery(event.target.value)}
+                    type='text' 
+                    placeholder='Search!' 
+                    className='SearchInput'/>
+                <button 
+                    type='submit' 
+                    className='SearchButton'>
+                        Search!
+                </button>
+            </form>
             <p>
-                <Link to={`/?query={"city": "Manchester"}`}>Manchester</Link>
+                <Link to={buildQueryString('query', {city: 'Manchester'})}>Manchester</Link>
             </p>
             <p>
-                <Link to={`/?query={"city": "Leeds"}`}>Leeds</Link>
+                <Link to={buildQueryString('query', {city: 'Leeds'})}>Leeds</Link>
             </p>
             <p>
-                <Link to={`/?query={"city": "Sheffield"}`}>Sheffield</Link>
+                <Link to={buildQueryString('query', {city: 'Sheffield'})}>Sheffield</Link>
             </p>
             <p>
-                <Link to={`/?query={"city": "Liverpool"}`}>Liverpool</Link>
+                <Link to={buildQueryString('query', {city: 'Liverpool'})}>Liverpool</Link>
             </p>
             <p>
-                <Link to={`/?query={"city": "Liverpool"}`}>Ascending</Link>
+                <Link to={buildQueryString('sort',  { price: 1 })}>Price Ascending</Link>
             </p>
             <p>
-                <Link to={`/?query={"city": "Liverpool"}`}>Descending</Link>
+                <Link to={buildQueryString('sort', { price: -1 })}>Price Descending</Link>
             </p>
         </div>
 
     )
    
 }
+
 
 export default SideBar
