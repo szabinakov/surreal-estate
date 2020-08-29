4c15dd2fe25d3c0f18a75cf2c2f700aea8d50bcd
szabinakov <szabinkov@gmail.com>
2020-08-29 18:56:19 +0100

    added Facebook login to navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/App.css';
 import { Switch, Route } from 'react-router-dom'
 
@@ -7,9 +7,18 @@ import Properties from './Properties'
 import AddProperty from './AddProperty'
 
 function App() {
+  const [userID, setUserID] = useState('')
+
+  const handleLogin = (response) => {
+    setUserID(response.userID)
+  }
+
+  const handleLogout = () => {
+    window.FB.logout((response) => setUserID(''))}
+
   return (
     <>
-      <NavBar/>
+      <NavBar userID={userID} onLogin={handleLogin} onLogout={handleLogout}/>
       <Switch>
         <Route exact path='/' component={Properties}/>
         <Route path='/add-property' component={AddProperty}/>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,9 @@ import React from 'react'
 import '../styles/NavBar.css'
 import logo from '../logo.png'
 import { Link } from 'react-router-dom'
+import FacebookLogin from 'react-facebook-login'
 
-const NavBar = () => {
+const NavBar = ({userID, onLogin, onLogout}) => {
     return(
 
         <div className='navbar'>
@@ -16,6 +17,17 @@ const NavBar = () => {
                     <Link className='navbar-links-item' to='/add-property'>Add Property</Link>
                 </li>
             </ul>
+            <div>
+                {userID
+                ?   <button onClick={onLogout} className='FacebookLogout'>Log Out</button>
+                :   <FacebookLogin
+                        appId="230725981691233"
+                        autoLoad={true}
+                        fields="name,email,picture"
+                        callback={onLogin}
+                        className='FacebookLogin'
+                />}
+                </div>
         </div>
 
     )
