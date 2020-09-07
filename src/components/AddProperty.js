d819cc5f3e77a443098064202c4a7e0806df2260
szabinakov <szabinkov@gmail.com>
2020-09-07 15:03:38 +0100

    debug

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -27,7 +27,7 @@ const AddProperty = () => {
     const handleAddProperty = (event) => {
         event.preventDefault()
         setAlert({message:'', isSuccess:false})
-        axios.post('https://localhost:4000/api/v1/PropertyListing', fields)
+        axios.post('https://surreal-estate-iota.vercel.app/api/v1/PropertyListing', fields)
         .then(() => { 
             setAlert({ message:'Property Added', isSuccess:true})
         })
